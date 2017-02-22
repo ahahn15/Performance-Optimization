2843dad242d6085eab2fe92ec3c7c3778e12068b
Annie Hahn <annie.hahn@apexlearning.com>
2017-02-21 20:27:30 -0800

    add htmlmin and minify index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,17 +46,30 @@ module.exports = function(grunt) {
           ext: '.min.css'
         }]
       }
+    },
+    htmlmin: {
+    dist: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      files: {
+        'dist/index.html': 'src/index.html'
+      }
     }
-  });
+  }
+});
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
   grunt.registerTask('default', ['imagemin']);
   grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('default', ['htmlmin']);
 
 };
