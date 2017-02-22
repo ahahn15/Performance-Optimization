37a63dde9700febda16034cb549cf12ad850cb27
Annie Hahn <annie.hahn@apexlearning.com>
2017-02-21 20:15:00 -0800

    add cssmin and minify CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,15 +35,28 @@ module.exports = function(grunt) {
           dest: 'dist/img/'
         }]
       }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.min.css'
+        }]
+      }
     }
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
   grunt.registerTask('default', ['imagemin']);
+  grunt.registerTask('default', ['cssmin']);
 
 };
